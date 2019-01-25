815be2bc3361dabf1b29a48ae96f5a0f38fdeb9c
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-01-25 18:16:38 +0000

    Initial commit with the project structure skeleton code.
    Further description: initialised Nuxt.js universal app.
    Setup has the following features skeleton:
    1. Vuetify UI lib
    2. Vue router
    3. Jest for test cases
    4. ESlint for linting
    5. Axios for http requests
    6. Prettier for automatic code refactoring
    7. Scss/Stylus support
    8. Webpack template.

---
+++ README.md
@@ -0,0 +1,22 @@
+# flatfair_frontend_challenge
+
+> Nuxt/vue.js-based Flatbond platform interface prototype made as part of the recrutation frontend challenge  
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ layouts/default.vue
@@ -0,0 +1,81 @@
+<template>
+	<v-app dark>
+		<v-navigation-drawer mini-variant
+		 v-model="drawer"
+		 permanent
+		 app>
+			<v-list>
+				<v-list-tile v-for="(item, i) in items"
+				 :to="item.to"
+				 :key="i"
+				 router
+				 exact>
+					<v-list-tile-action>
+						<v-icon v-html="item.icon" />
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title v-text="item.title" />
+					</v-list-tile-content>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+		<v-toolbar fixed
+		 dense
+		 app>
+			<v-toolbar-title>
+				FLATBOND
+			</v-toolbar-title>
+		</v-toolbar>
+		<v-content id='main-background-window'>
+			<v-container>
+				<nuxt />
+			</v-container>
+		</v-content>
+		<v-navigation-drawer :right="right"
+		 v-model="rightDrawer"
+		 temporary
+		 fixed>
+			<v-list>
+				<v-list-tile @click.native="right = !right">
+					<v-list-tile-action>
+						<v-icon light>compare_arrows</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+		<v-footer :fixed="fixed"
+		 app>
+			<a class='caption'></a>
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageURL: null,
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      mini: true,
+      items: [
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Main layout'
+    }
+  },
+  methods: {},
+  computed: {},
+  mounted() {}
+}
+</script>
+
+<style scoped>
+</style>
+
