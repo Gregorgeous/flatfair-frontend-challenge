2c58e85ed0093de8a8cf52b96a644363b01cf274
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-01-29 21:17:30 +0000

    Added my main theme colours aliases to the vuetify plugin.
    Further description: these can be used in the
    'color' attribute on any Vuetify UI element.

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,9 +4,9 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#121212', // a color that is not in the material colors palette
+    primary: '#051033', // a color that is not in the material colors palette
     accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
+    secondary: '#6785FF',
     info: colors.teal.lighten1,
     warning: colors.amber.base,
     error: colors.deepOrange.accent4,
