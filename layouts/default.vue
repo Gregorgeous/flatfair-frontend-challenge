85774b2e02bb1c4a339515e226bb562bad80bdee
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-01-29 20:16:41 +0000

    (Creating layout page #1) Created markup for the navbar with BEM-system class style names.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,25 @@
 <template>
 	<v-app light>
+		<nav class="mainNav">
+			<v-toolbar class="mainNav__toolbar"
+			 fixed
+			 app>
+				<div class="website-title">
+					<FlatfairLogoHomeSvg class='website-title__svg-logo--home'></FlatfairLogoHomeSvg>
+					<KnotLogoSvg class='website-title__svg-logo--knot hidden'></KnotLogoSvg>
+					<FLATSvg class='website-title__svg-logo--flat'></FLATSvg>
+					<FAIRSvg class='website-title__svg-logo--fair'></FAIRSvg>
+					<BONDSvg class='website-title__svg-logo--bond hidden'></BONDSvg>
+				</div>
+			</v-toolbar>
+			<v-btn class="mainNav__home-icon"
+			 flat
+			 icon
+			 href="https://flatfair.co.uk/"
+			 color="primary">
+				<v-icon large>home</v-icon>
+			</v-btn>
+		</nav>
 		<v-content id='main-background-window'>
 			<v-container>
 				<nuxt />
