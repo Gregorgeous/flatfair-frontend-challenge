670f0ea35df96ea37cc639114a886dfbac886752
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-01-29 21:23:00 +0000

    (Creating Vuex store #1): Create the State of our Vuex store.
    Gentle pointer to the "IDEA" tags for more information :)

--- store/index.js
+++ store/index.js
@@ -1,4 +1,17 @@
-export const state = () => ({})
+export const state = () => ({
+  isFlatbondCreated: true,
+  flatbondDetails: {
+    //   IDEA: This one below could be easily swapped with just a boolean for a minor performance win, but I decided to do explicit string check for a better code readability.
+    // Also IDEA: I filled these with dummy data for convenience, but those will be overriden with the actual data on form submission anyway
+    whichRentArrangementChosen: 'weekly',
+    monthlyRentAmount: 2000,
+    weeklyRentAmount: 120,
+    postcode: 'SE2 xxx',
+    membershipFee: 120
+  },
+  fixedMembershipFee: false,
+  fixedMembershipFeeAmount: null
+})
 
 export const mutations = {}
 
