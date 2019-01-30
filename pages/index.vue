4ec233f231d405eeb9d1edee5c3210917b4fdcb9
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-01-30 00:23:04 +0000

    (MAJOR:Creating first page #10) Created the whole markup for the form.
    NOTICE a couple of interesting points:
    - Conditional/dynamic ":Class" attributes; If the 'formCreatedSuccessful'
    variable is True then various form elements will have a special class applied
    to them that will make them highlighted in green (to be implemented)
    - @clicks on the "__choose--box" elements; we have two btns,
    depending on which we click we tell the function "chooseBoxBtnClicked"
    whether it's the 'weekly' or 'monthly' arrangement btn.
    That changes things in our data() variables and triggers either of
    the "weekly" or "monthly" input fields to show via the "v-if" that
    handles their visibility.
    - the <v-icon> tags are Vuetify's built-in components that are
    configured to show the Google's Material Design Icon. It'll show
    the icon that the name of we specify in the tag content
    (e.g. "add_circle_outline" etc.)
    - ":rules" attributes in <v-text-field> components; these attributes
    come baked into Vuetify's components inside the <v-form>
    (e.g. v-text-field). We can specify the validation rules for the given
    user's input.
    NB. I have specified some of the rules inside the markup and some
    (postcode rules) inside data. This is because if you specify it
    inside data, you lose the "this" scope (imho that's weird) , which I need
    for both the weekly and monthly rent value inputs.
    - The rest should be pretty self-explanatory.

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,102 @@
 		 sm8
 		 md6>
 			<div class="text-xs-center">
+		<v-form ref="form"
+		 class="flatbond-form"
+		 :class="formCreatedSuccesfully?'form-successful':''"
+		 v-model="valid">
+			<div class='flatbond-form__circle'
+			 :class="formCreatedSuccesfully?'form-successful-3':''">
+				<FlatfairLogoHomeSvg v-if="!formCreatedSuccesfully"
+				 :class="formCreatedSuccesfully?'form-successful-4':''"
+				 v-model="valid"
+				 class="flatbond-form__circle--logo-svg"></FlatfairLogoHomeSvg>
+				<h1 :class="formCreatedSuccesfully?'form-successful-4':''"
+				 v-if="!formCreatedSuccesfully">Create Flatbond</h1>
+				<greenTickIcon :class="formCreatedSuccesfully?'form-successful-5':''"
+				 v-if="formCreatedSuccesfully"
+				 class="flatbond-form__circle--logo-svg"></greenTickIcon>
+			</div>
+			<h2 class='flatbond-form__rent-arrangement'>Rent arrangement</h2>
+			<div class="flatbond-form__choose-box">
+				<div class="flatbond-form__choose-box--weekly"
+				 :class="[weekly ? 'clicked' : 'un-clicked']"
+				 @click="chooseBoxBtnClicked('weekly')">
+					<h3>weekly</h3>
+				</div>
+				<span class='flatbond-form__choose-box--divider'></span>
+				<div class="flatbond-form__choose-box--monthly"
+				 :class="[monthly ? 'clicked' : 'un-clicked']"
+				 @click="chooseBoxBtnClicked('monthly')">
+					<h3>monthly</h3>
+				</div>
+			</div>
+			<v-text-field class='flatbond-form__input--monthly-arrangement'
+			 label="Please enter a range between £2000 - £8660"
+			 placeholder='2000'
+			 outline
+			 :rules="[ () => {return weeklyOrMonthlyValue>=2000 && weeklyOrMonthlyValue<=8660  || 'Please provide a proper range'}]"
+			 background-color='secondary'
+			 prefix="£"
+			 v-model="weeklyOrMonthlyValue"
+			 v-if="monthly"></v-text-field>
+			<v-text-field class='flatbond-form__input--weekly-arrangement'
+			 label="Please enter a range between £25 - £110"
+			 placeholder='25'
+			 outline
+			 :rules="[() => {return weeklyOrMonthlyValue>=25 && weeklyOrMonthlyValue<=110  || 'Please provide a proper range'}]"
+			 background-color='secondary'
+			 prefix="£"
+			 v-model="weeklyOrMonthlyValue"
+			 v-if="weekly"></v-text-field>
+			<v-icon x-large
+			 color="secondary">add_circle_outline</v-icon>
+			<h4 class="flatbond-form__membership--heading">One-off membership fee</h4>
+			<v-text-field class='flatbond-form__input--membership-fee'
+			 value="144"
+			 single-line
+			 v-model="membershipFee"
+			 background-color='secondary white--text'
+			 readonly
+			 prefix="£"
+			 dark
+			 solo></v-text-field>
+			<h2 class="flatbond-form__postcode--heading">Postcode</h2>
 
+			<div class="flatbond-form__input--postcode"></div>
+			<v-text-field class='flatbond-form__input--postcode'
+			 placeholder='e.g. SE14 6NW '
+			 outline
+			 :rules='postcodeRules'
+			 single-line
+			 v-model="postcodeValue"
+			 background-color='secondary'
+			 required></v-text-field>
+
+			<div class='flatbond-form__btns'>
+				<v-btn class='flatbond-form__btns--clear-btn'
+				 @click="clearForm"
+				 small
+				 fab
+				 color="white">
+					<v-icon id="trashIcon">delete_outline</v-icon>
+				</v-btn>
+				<!--IDEA: I use dynamic ID assigning to overcome vuetify's stubborn styling (look up near #submitBtn--disabled in styling for an 'IDEA' about it.) -->
+				<v-btn @click="submitFlatbondForm"
+				 :id="valid ? 'submitBtn--valid' : 'submitBtn--disabled'"
+				 round
+				 large
+				 class='flatbond-form__btns--submit-btn'
+				 :class="formCreatedSuccesfully?'form-successful-2':''"
+				 color="secondary"
+				 :disabled="!valid"
+				 :loading="isLoading"
+				 dark>Submit</v-btn>
 			</div>
 		</v-flex>
+
+		</v-form>
+
 		<v-snackbar v-model="snackbar"
 		 multi-line
 		 bottom
